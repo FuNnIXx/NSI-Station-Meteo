efe280e75e168b5550a7e4dd94d98b453321274f
FuNnIXx <143885250+FuNnIXx@users.noreply.github.com>
2025-12-03 09:50:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 # Import the requests module
 import requests
 from tkinter import*
+from iso_3166 import*
 
 def DataGet():
+    Pays = ISO_CTR(input("Pays de recherche : "))
     cityName = input("Nom de la ville : ")
     if ' ' in cityName :
         cityName = cityName.replace(' ', '+')
-
-    url = ('https://api.openweathermap.org/data/2.5/weather?q=###,fr&appid=7585221fbcead099c4e4c4bb6fd3b68f')
+    url = ('https://api.openweathermap.org/data/2.5/weather?q=###,++&appid=7585221fbcead099c4e4c4bb6fd3b68f')
     url = url.replace('###', cityName)
-    # Send a GET request to the desired API URL
+    url = url.replace('++', Pays)
+
     response = requests.get(url)
+    response = response.json()
+    if response['cod'] == '404':
+        return response('message')
+    else:
+        return(response)
 
-    # Parse the response and print it
-    reponse = response.json()
+DATA = DataGet()
 
-DataGet()
+openstreetmap = 'https://www.openstreetmap.org/relation/108394#map=13/###/+++'
+openstreetmap = openstreetmap.replace('###', str(DATA['coord']['lat']))
+openstreetmap = openstreetmap.replace('+++', str(DATA['coord']['lon']))
+
+Weather = DATA[]
