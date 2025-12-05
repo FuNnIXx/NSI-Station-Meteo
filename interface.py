16f300343726fdca78bcc36e49bd5ed79ef52a3a
malom <malomerieux@hotmail.com>
2025-12-05 23:54:00 +0100

    Merge remote-tracking branch 'origin/main'

--- interface.py
+++ interface.py
@@ -1,28 +1,61 @@
-from tkinter import *
-from customtkinter import *
+import requests
+import difflib
+import dict
+import tkinter as tk
 from tkinter import messagebox
-from tkinter import ttk
 import webbrowser
 
-from customtkinter import CTkButton
+city = '' # Nom de la ville de recherche
+country = '' # Nom du pays de recherche
+countryiso = '' # Nom du pays de recherche sous la forme ISO_3166 2C
 
-root = CTk()
-root.title("Weather's Call")
-root.geometry("1000x600")
-root.resizable(False, False)
-root.config(bg="#252530")
+ctr_search = ''
+city_data = ''
+iso_search = ''
 
-menu = CTkFrame(root, border_color='#202029', )
-menu.place(relx=0.01, rely=0.02, relwidth=0.30, relheight=0.96)
-menu.grid_rowconfigure(0, weight=1)
+data = '' # Données météo de la ville
 
-label = CTkLabel(menu, text="WeatherDATA", padx=5, pady=5, font=("Monserrat", 30, 'bold'), text_color='white')
-label.place(relx=0.15, rely=0.055, relwidth=0.7, relheight=0.05)
+def get_data(countryiso, city): # Retourne les données météo de la ville, sous la forme d'un dictionnaire
+    if ' ' in city:
+        city = city.replace(' ', '+')
+    url = (
+        'https://api.openweathermap.org/data/2.5/weather?q=###,++&appid=7585221fbcead099c4e4c4bb6fd3b68f&units=metric')
+    url = url.replace('###', city)
+    url = url.replace('++', countryiso)
+    data = requests.get(url)
+    data = data.json()
+    if data['cod'] == '404':
+        return('error 404 : City not found')
+    else:
+        return(data)
 
-search_field = CTkEntry(menu, placeholder_text="Pays ...")
-search_field.place(relx=0.15, rely=0.15, relwidth=0.7, relheight=0.05)
-search_button = CTkButton(menu, text='Search')
+def get_map_link(data): # Retourne le lien de la carte de la ville
+    openstreetmap = 'https://www.openstreetmap.org/#map=13/###/+++'
+    openstreetmap = openstreetmap.replace('###', str(data['coord']['lat']))
+    openstreetmap = openstreetmap.replace('+++', str(data['coord']['lon']))
+    return(openstreetmap)
+
+def country_search(ctr_search): # Retourne la liste des pays correspondants à la recherche
+    global ctr_research
+    list = dict.ctr_to_iso()
+    ctr_research = difflib.get_close_matches(ctr_search, list.keys(), n=10, cutoff=0.5)
+
+def country_to_iso(country): # Retourne le code ISO_3166
+    global countryiso
+    list = dict.ctr_to_iso()
+    countryiso = list[country]
+
+def city_data_get(country): # Retourne la liste des villes correspondantes à la recherche
+    global city_data
+    url = "https://countriesnow.space/api/v0.1/countries/cities"
+    payload = {"country": country}
+    headers = {}
+    city_data = requests.request("POST", url, headers=headers, data=payload)
+
+def city_search():
+    global city
+    list = city_data.json()
+    city = difflib.get_close
 
 
-root.mainloop()
-
+# Il faut tout refaire, c'est bon pour du cli mais pas pour TK
