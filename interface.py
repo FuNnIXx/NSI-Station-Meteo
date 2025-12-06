67b922f56c6affd44e46d1fe977de783b9a14651
malom <malomerieux@hotmail.com>
2025-12-06 19:02:23 +0100

    Merge remote-tracking branch 'origin/main'

--- interface.py
+++ interface.py
@@ -1,61 +1,151 @@
-import requests
-import difflib
-import dict
-import tkinter as tk
-from tkinter import messagebox
+from customtkinter import *
+from CTkListbox import *
+import messagebox
 import webbrowser
 
-city = '' # Nom de la ville de recherche
-country = '' # Nom du pays de recherche
-countryiso = '' # Nom du pays de recherche sous la forme ISO_3166 2C
+'''VARIABLES DE DEFINITIONS'''
 
-ctr_search = ''
-city_data = ''
-iso_search = ''
+color_bg = '#292933'
+color_frame = '#202029'
 
-data = '' # Données météo de la ville
+data = False
 
-def get_data(countryiso, city): # Retourne les données météo de la ville, sous la forme d'un dictionnaire
-    if ' ' in city:
-        city = city.replace(' ', '+')
-    url = (
-        'https://api.openweathermap.org/data/2.5/weather?q=###,++&appid=7585221fbcead099c4e4c4bb6fd3b68f&units=metric')
-    url = url.replace('###', city)
-    url = url.replace('++', countryiso)
-    data = requests.get(url)
-    data = data.json()
-    if data['cod'] == '404':
-        return('error 404 : City not found')
-    else:
-        return(data)
+city_name = False
+ctr_name = False
 
-def get_map_link(data): # Retourne le lien de la carte de la ville
+temp = 0
+temp_min = 0
+temp_max = 0
+fells_like = 0
+pressure = 1024
+wind = 0
+visibility = 0
+cloud = 0
+humidity = 0
+lon = 0
+lat = 0
+
+
+'''FONCTIONS'''
+
+# OpenSteetMap Button
+
+def open_map():
+    if not city_name or not ctr_name or not data:
+        messagebox.showerror('Error', "Effectuez une recherche avant d'ouvrir la carte")
+        return()
     openstreetmap = 'https://www.openstreetmap.org/#map=13/###/+++'
-    openstreetmap = openstreetmap.replace('###', str(data['coord']['lat']))
-    openstreetmap = openstreetmap.replace('+++', str(data['coord']['lon']))
-    return(openstreetmap)
+    openstreetmap = openstreetmap.replace('###', f'{lat}')
+    openstreetmap = openstreetmap.replace('+++', f'{lon}')
+    if openstreetmap:
+        webbrowser.open(openstreetmap)
 
-def country_search(ctr_search): # Retourne la liste des pays correspondants à la recherche
-    global ctr_research
-    list = dict.ctr_to_iso()
-    ctr_research = difflib.get_close_matches(ctr_search, list.keys(), n=10, cutoff=0.5)
+'''INTERFACE UI'''
 
-def country_to_iso(country): # Retourne le code ISO_3166
-    global countryiso
-    list = dict.ctr_to_iso()
-    countryiso = list[country]
+# ROOT
 
-def city_data_get(country): # Retourne la liste des villes correspondantes à la recherche
-    global city_data
-    url = "https://countriesnow.space/api/v0.1/countries/cities"
-    payload = {"country": country}
-    headers = {}
-    city_data = requests.request("POST", url, headers=headers, data=payload)
+root = CTk()
 
-def city_search():
-    global city
-    list = city_data.json()
-    city = difflib.get_close
+root.title("WeatherDATA")
+root.geometry("1100x700")
+root.resizable(False, False)
+root.config(bg=color_bg)
+root.iconbitmap('logo.ico')
 
+# FRAMING
 
-# Il faut tout refaire, c'est bon pour du cli mais pas pour TK
+frame = CTkFrame(root, fg_color=color_bg, corner_radius=0)
+frame.pack(side=TOP, fill=BOTH, expand=True, pady=7.5, padx=7.5)
+
+menu = CTkFrame(frame, fg_color=color_frame, width=300, corner_radius=10)
+menu.pack(side=LEFT, fill=BOTH, pady=7.5, padx=7.5)
+
+right_side = CTkFrame(frame, fg_color=color_bg, corner_radius=0)
+right_side.pack(side=RIGHT, fill=BOTH, expand=True, pady=7.5, padx=7.5)
+
+title_frame = CTkFrame(right_side, fg_color=color_frame, height=70, corner_radius=10) # TOP FRAME CITY TITLE + OPENSTREETMAP BUTTON
+title_frame.pack(side=TOP, fill=BOTH)
+
+space1 = CTkFrame(right_side, fg_color=color_bg, corner_radius=0, height=15) # A DEFAUT DE PADY, UNE FRAME TRANSPARENTE
+space1.pack(side=TOP, fill=BOTH)
+
+general_info = CTkFrame(right_side, fg_color=color_frame, corner_radius=10, height=180) # MID FRAME INFO MERE
+general_info.pack(side=TOP, fill=BOTH)
+
+space2 = CTkFrame(right_side, fg_color=color_bg, corner_radius=0, height=15) # A DEFAUT DE PADY, UNE FRAME TRANSPARENTE
+space2.pack(side=TOP, fill=BOTH)
+
+precisions_info = CTkFrame(right_side, fg_color=color_frame, corner_radius=10)
+precisions_info.pack(side=TOP, fill=BOTH, expand=YES)
+
+# Cadres titres verticaux General et Precision
+
+title_info_1_frame = CTkFrame(general_info, fg_color='white', corner_radius=100, width=25) # Cadre titre general
+title_info_1_frame.pack(side=LEFT, fill=BOTH, pady=10, padx=12)
+title_info_1_frame.pack_propagate(False)
+
+title_info_2_frame = CTkFrame(precisions_info, fg_color='white', corner_radius=100, width=25) # Cadre titre precisions
+title_info_2_frame.pack(side=LEFT, fill=BOTH, pady=10, padx=12)
+title_info_2_frame.pack_propagate(False)
+
+# TITLE FRAME
+
+header = CTkLabel(title_frame, text=f'Données météo à {city_name} ', font=('Monserrat', 30, 'bold'), text_color='white', bg_color='transparent')
+header.pack(pady=10, padx=15, side=LEFT)
+
+openstreetmap_button = CTkButton(title_frame, text='OpenStreetMap', font=('Monserrat', 15), text_color='#59608C', corner_radius=10, fg_color='white', hover_color='#B6B6B6', command=open_map)
+openstreetmap_button.pack(pady=10, padx=15, side=RIGHT)
+
+# MERNU FRAME
+
+app_title = CTkLabel(menu, text='WeatherDATA', font=('Monserrat', 30, 'bold'), width=300)
+app_title.pack(pady=30, padx=0, side=TOP, fill=BOTH)
+
+# RECHERCHE DU PAYS
+
+ctr_search_frame = CTkFrame(menu, fg_color=color_frame)
+ctr_search_frame.pack(side=TOP, fill=BOTH, pady=7.5, padx=15)
+
+ctr_entry = CTkEntry(ctr_search_frame, placeholder_text='Pays ...', corner_radius=10, width=175, height=35, fg_color='white', text_color='#59608C')
+ctr_entry.pack(fill=BOTH, pady=0, padx=0, side=LEFT)
+
+ctr_button = CTkButton(ctr_search_frame, text='VALIDER', corner_radius=10, font=('Monserrat', 15, 'bold'), hover_color='#B6B6B6', text_color='#59608C', fg_color='white', width=80) # BOUTON VALIDER PAYS
+ctr_button.pack(fill=BOTH, pady=0, padx=0, side=RIGHT)
+
+ctr_list = CTkListbox(menu, width=2)
+ctr_list.pack(side=TOP, fill=BOTH, expand=YES,pady=7.5, padx=15)
+
+# RECHERCHE DE LA VILLE
+
+city_search_frame = CTkFrame(menu, fg_color=color_frame)
+city_search_frame.pack(side=TOP, fill=BOTH, pady=7.5, padx=15)
+
+city_entry = CTkEntry(city_search_frame, placeholder_text='Ville ...', corner_radius=10, width=175, height=35, fg_color='white', text_color='#59608C')
+city_entry.pack(fill=BOTH, pady=0, padx=0, side=LEFT)
+
+city_button = CTkButton(city_search_frame, text='VALIDER', corner_radius=10, font=('Monserrat', 15, 'bold'), hover_color='#B6B6B6', text_color='#59608C', fg_color='white', width=80) # BOUTON VALIDER PAYS
+city_button.pack(fill=BOTH, pady=0, padx=0, side=RIGHT)
+
+city_list = CTkListbox(menu, width=2)
+city_list.pack(side=TOP, fill=BOTH, expand=YES,pady=7.5, padx=15)
+
+space3 = CTkFrame(menu, fg_color=color_frame, corner_radius=0, height=7.5)
+space3.pack()
+
+# GENERAL INFO FRAME
+
+# GENERAL FRAME NAME
+
+canvas_title_general = CTkCanvas(title_info_1_frame, highlightthickness=0)
+canvas_title_general.pack(pady=10, padx=1, expand=YES)
+canvas_title_general.create_text(12, 90, text="GENERAL", font=("Monserrat", 15, "bold"), fill="#000000", angle=90)
+
+# PRECISION INFO FRAME
+
+# PRECISION FRAME NAME
+
+canvas_title_precision = CTkCanvas(title_info_2_frame, highlightthickness=0)
+canvas_title_precision.pack(pady=10, padx=1, expand=YES)
+canvas_title_precision.create_text(12, 132, text="PRECISIONS", font=("Monserrat", 15, "bold"), fill="#000000", angle=90)
+
+root.mainloop()
